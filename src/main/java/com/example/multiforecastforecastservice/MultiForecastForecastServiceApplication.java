8f8b9908d7097b46c1c652358ec50f0982104d23
AndrejKlim <andrei.klimovich98@mail.ru>
2022-10-26 11:21:40 +0300

    Initial commit

---
+++ src/main/java/com/example/multiforecastforecastservice/MultiForecastForecastServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.multiforecastforecastservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiForecastForecastServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiForecastForecastServiceApplication.class, args);
+    }
+
+}
